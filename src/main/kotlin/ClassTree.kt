0f19fac5e30a1b2ad15630220762a9c3b4373e48
Young Moon KO <youngmoon.ko@doordash.com>
2023-10-22 18:50:12 -0700

    add box color for interface

--- src/main/kotlin/ClassTree.kt
+++ src/main/kotlin/ClassTree.kt
@@ -167,7 +167,7 @@ object ClassTree {
 
             val classNode = getClassNode(className)
             if (classNode?.isInterface == true) {
-                sb.append(" $from [color=$INTERFACE_COLOR,style=filled,shape=rect];\n")
+                sb.append(" $from [color=black,fillcolor=$INTERFACE_COLOR,style=filled,shape=rect];\n")
             } else {
                 sb.append(" $from [color=$color,style=filled];\n")
             }
